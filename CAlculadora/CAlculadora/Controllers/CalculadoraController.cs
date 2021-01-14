43be923c310c56cae5c3998717c0ddc7bc18e854
victoriosote <a2019112604@estudiantes.upsa.edu.bo>
2021-01-14 19:44:42 -0400

    suma en get post

---
+++ CAlculadora/CAlculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CAlculadora.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public int sumaget(int a, int b)
+        {
+            return a + b;
+        }
+        [HttpPost]
+        public int sumapost([FromHeader] int a, [FromHeader] int b)
+        {
+            return a + b;
+        }
+    }
+}
