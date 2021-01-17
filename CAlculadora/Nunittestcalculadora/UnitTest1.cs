27cf48c43aaf38d9523ca01f1a3453a0f7dda899
victoriosote <a2019112604@estudiantes.upsa.edu.bo>
2021-01-16 20:57:41 -0400

    Pruebas de resta y multi en get y post respectivamente

--- CAlculadora/Nunittestcalculadora/UnitTest1.cs
+++ CAlculadora/Nunittestcalculadora/UnitTest1.cs
@@ -96,5 +96,176 @@ namespace Nunittestcalculadora
             //Assert=verification
             Assert.AreEqual(esperado, resultado);
         }
+        [Test]
+        public void TestResta1()
+        {
+            //Arrange=preparacion
+            int num1 = 8;
+            int num2 = 7;
+            int esperado = 1;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.restapost(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+
+        }
+        [Test]
+        public void TestResta2()
+        {
+            //Arrange=preparacion
+            int num1 = -8;
+            int num2 = 5;
+            int esperado = -13;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.restapost(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestResta3()
+        {
+            //Arrange=preparacion
+            int num1 = -6;
+            int num2 = -3;
+            int esperado = -3;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.restapost(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestRestaGet1()
+        {
+            //Arrange=preparacion
+            int num1 = 1;
+            int num2 = 6;
+            int esperado = -5;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.restaget(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+
+        }
+        [Test]
+        public void TestRestaGet2()
+        {
+            //Arrange=preparacion
+            int num1 = -9;
+            int num2 = -8;
+            int esperado = -1;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.restaget(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestRestaGet3()
+        {
+            //Arrange=preparacion
+            int num1 = -8;
+            int num2 = -3;
+            int esperado = -5;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.restaget(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        public void TestMulti1()
+        {
+            //Arrange=preparacion
+            int num1 = 8;
+            int num2 = 7;
+            int esperado = 56;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.multipost(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+
+        }
+        [Test]
+        public void TestMulti2()
+        {
+            //Arrange=preparacion
+            int num1 = -8;
+            int num2 = 5;
+            int esperado = -40;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.multipost(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestMulti3()
+        {
+            //Arrange=preparacion
+            int num1 = -6;
+            int num2 = -3;
+            int esperado = 18;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.multipost(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestMultiGet1()
+        {
+            //Arrange=preparacion
+            int num1 = 1;
+            int num2 = 6;
+            int esperado = 6;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.multiget(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+
+        }
+        [Test]
+        public void TestMultiGet2()
+        {
+            //Arrange=preparacion
+            int num1 = -9;
+            int num2 = -8;
+            int esperado = 72;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.multiget(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
+        [Test]
+        public void TestMultiGet3()
+        {
+            //Arrange=preparacion
+            int num1 = -8;
+            int num2 = -3;
+            int esperado = 24;
+            CalculadoraController calc = new CalculadoraController();
+
+            //Act=ejecucion
+            int resultado = calc.multiget(num1, num2);
+            //Assert=verification
+            Assert.AreEqual(esperado, resultado);
+        }
     }
 }
